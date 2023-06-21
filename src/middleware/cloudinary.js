f71af36b6390eabea57821534b2e8a1dd849efa0
José María Lozano Olmedo <116385651+chemiya@users.noreply.github.com>
2023-06-21 14:33:00 +0200

    cloudinary

--- src/middleware/cloudinary.js
+++ src/middleware/cloudinary.js
@@ -4,9 +4,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  cloud_name: "chemareact",
+  api_key: "381482912163929",
+  api_secret: "JgSnPPHx860_oK74TJGG3nFhJfM"
 })//configuro la conexion conc loudinary
 
 
